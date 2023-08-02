6bbef168cc9a6bcd1b7782593c17feea1d6a3fe6
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2023-08-02 19:47:10 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <div class="container">
+      <img src="./assets/pinia-logo.svg" alt="Pinia" title="Pinia">
+      <h1>My Diary</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
