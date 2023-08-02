a529e4e7ed5299e9b38f3cdda2480b1652e188b6
Enes Can Aydemir <enescan_aydemir@hotmail.com>
2023-08-03 00:23:11 +0300

    css and buttons written

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,49 @@
 <template>
   <header>
     <div class="container">
-      <img src="./assets/pinia-logo.svg" alt="Pinia" title="Pinia">
+      <img src="./assets/pinia-logo.svg" alt="Pinia" title="Pinia" />
       <h1>{{ gunlukStore.name }}</h1>
     </div>
   </header>
 
   <main>
     <div class="container">
-      <div class="gunluk-listesi" v-for="gunluk in gunlukStore.diary">
 
+      <div class="data-area">
+        <p v-if="filter === 'all'">Toplam {{ gunlukStore.totalCount}} Günlük Var</p>
+        <p v-if="filter === 'favs'">Toplam {{ gunlukStore.favCount }} Favori Günlük Var</p>
+        <div>
+          <button @click="$event => filter='all'" >Tüm Günlüklerim</button>
+          <button @click="$event => filter='favs'">Sadece Beğendiklerim</button>
+        </div>
+      </div>
+
+      <div class="gunluk-listesi" v-for="gunluk in gunlukStore.diary" v-if="filter === 'all'"> <!--Sondaki if koşulu: aşağıda script içeriisnde tanımladığımız filter için buraya bir filtreleme koyduk. Eğer all ise tüm günlükleri gösterecek, favs ise beğendiğim günlükleri gösterecek. Bunun için yukarıya 2 buton ekledim ve içerisine click ekleyip seçildiğinde filter eventine göre listelenmesi için ekledim aynı burda yazıldığı gibi.  -->
         <gunluk-details :gunluk="gunluk"></gunluk-details>
+      </div>
 
-
+      <div class="gunluk-listesi" v-for="gunluk in gunlukStore.favs" v-if="filter === 'favs'">
+        <gunluk-details :gunluk="gunluk"></gunluk-details>
       </div>
     </div>
   </main>
 </template>
 
 <script>
-import GunlukDetails from './components/gunluk-details.vue'
-import { useDiaryStore } from './stores/diaryStore';
+import { ref } from 'vue';
+import GunlukDetails from "./components/gunluk-details.vue";
+import { useDiaryStore } from "./stores/diaryStore";
 
 export default {
   components: {
-    "gunluk-details": GunlukDetails
+    "gunluk-details": GunlukDetails,
   },
-  setup(){
+  setup() {
     const gunlukStore = useDiaryStore();
-    return { gunlukStore }
-  }
-}
+
+    const filter = ref('all') //beğenilen ve tüm liste filtrelemesi için bir filter değişkeni oluşturup referans değeri olarak tüm listeyi yazdırdık
+
+    return { gunlukStore, filter };
+  },
+};
 </script>
--- src/stores/diaryStore.js
+++ src/stores/diaryStore.js
@@ -4,7 +4,23 @@ export const useDiaryStore = defineStore("diaryStore", {
     state: () => ({
         diary: [
             {id: 1, diary: "Bugün erken kalktım...", date: "2.08.2023", isFav: false},
-            {id: 2, diary: "Bugün  iş için Pinia tutorial izledim..", date: "3.08.2023", isFav: false},
+            {id: 2, diary: "Bugün  iş için Pinia tutorial izledim..", date: "3.08.2023", isFav: true},
         ]
-    })
+    }),
+    getters: { //getters objesi
+        favs(){ //method
+            return this.diary.filter(gunluk => gunluk.isFav) //sadece isFav ı true olanları aldık ve bunları gönderdik yani return ettik.
+        },
+        favCount(){ //beğenilen günlüklerin sayılarını aldı. 
+            return this.diary.reduce((previous, current) => {
+                return current.isFav ? previous + 1 : previous //Koşul olarak beğendiyse eğer previous değerini 1 arttır yazdık
+            }, 0)
+        },
+        totalCount: (state) => { //total sayıyı aldık. burda parantez içerisinde state tanımlamamızın sebebi, arrow funct kullandığımız için this o funct içerisindekini tanıyabiliyor ondan dışarı çıkamıyor. o yüzden bizde state tanımlayığ this yerine state yazarak içerisinden diary'i aldık.
+            return state.diary.length
+        }
+    }
 })
+
+//getters: state içerisinde bulunan değişkenin içerisindeki herhangi bir parametreyi veya bu parametrenin değerine göre bir filtreleme yaparak sonucu göstermemizi sağlayan öğeler 
+//yukarıda getters kullanmamızın sebebi; getters, dizinin içerisindeki parametreyi hazır olarak çekip ve sadece o parametreyi dönebiliyor. Yani komple diziyi alıp içerisinden ayıklamak zaman alır ve gereksiz yer kaplar. Bizde diary içerisinde tek tek bulmak yerine parametre şeklinde alıp (isFav aldık yukarıda) onu kullancağız.
